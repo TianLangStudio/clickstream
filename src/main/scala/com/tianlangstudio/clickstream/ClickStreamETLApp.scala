3445120d280382eb38ea34c30c183482d52519a8
TianlangStudio <tianlangstudio@aliyun.com>
2019-08-03 16:32:58 +0800

    KafkaProducer

--- src/main/scala/com/tianlangstudio/clickstream/ClickStreamETLApp.scala
+++ src/main/scala/com/tianlangstudio/clickstream/ClickStreamETLApp.scala
@@ -4,12 +4,15 @@ import java.util
 
 import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark._
 import org.apache.spark.streaming._
+
+import scala.collection.JavaConverters._
 
 object ClickStreamETLApp {
   val logNginxPattern = """(\d+.\d+.\d+.\d+)\^([-\w\d]+)\^\[(.*)\]\^"(\w+) /stat.png\?([^\^]+)& (\w+/[\d.]*)"\^\d*\^\d*\^"[^"]*"\^"[^"]*"\^"[^"]*"""".r
@@ -26,6 +29,8 @@ object ClickStreamETLApp {
       "bootstrap.servers" -> "192.168.0.102:9092",
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
+      "key.serializer" -> classOf[StringSerializer],
+      "value.serializer" -> classOf[StringSerializer],
       "group.id" -> "click_stream_etl",
       "auto.offset.reset" -> "latest",
       "enable.auto.commit" -> (false: java.lang.Boolean)
@@ -47,7 +52,22 @@ object ClickStreamETLApp {
         s"""$sid$fieldDeli$timeLocal$fieldDeli$remoteAddr$fieldDeli$remoteUser$fieldDeli$reqMethod$fieldDeli$queryString$fieldDeli$schema"""
       }
       case _ => ""
-    }).filter(!"".equals(_)).print()
+    }).filter(!"".equals(_)).foreachRDD(strRdd => {
+      strRdd.foreachPartition(strI => {
+        val kafkaProducer = new KafkaProducer[String, String](kafkaParams.asJava)
+        try {
+          strI.foreach(value => {
+            val producerRecord = new ProducerRecord(etlTopic,"",value)
+            kafkaProducer.send(producerRecord)
+          })
+        }finally {
+          if(kafkaProducer != null) {
+            kafkaProducer.close()
+          }
+        }
+
+      })
+    })//.print()
     ssc.start()
     ssc.awaitTermination()
   }
